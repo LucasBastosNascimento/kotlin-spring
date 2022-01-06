59453f3ecde6375a79e44180146dc4cc26de4da6
lucas bastos <lucas.bastos.nas@gmail.com>
2022-01-06 19:33:55 -0300

    wip(kotlin): secao 4 - Variaveis

---
+++ fundamentos/src/main/kotlin/fundamentos/variaveis.kt
@@ -0,0 +1,25 @@
+package fundamentos
+
+fun main(){
+    var nomeVar = "Lucas"
+    val nomeVal = "Lucas"
+
+    nomeVar = "Lucas"
+//    variaveis val apoz atribuir um valor  não podem receber outro valor
+//    nomeVal ="Lucas"
+
+    val idade = 24
+    println(idade)
+
+    val teste: String
+
+}
+class variavel{
+    lateinit var teste:String
+
+    fun iniciaVariaveis(){
+        teste="teste"
+    }
+
+}
+
