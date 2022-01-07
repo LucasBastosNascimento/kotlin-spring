ca3a3e4c83ccf8ae7d3b87c0ce3fc675ab62efbf
lucas bastos <lucas.bastos.nas@gmail.com>
2022-01-07 18:20:16 -0300

    wip(kotlin): secao 4 - Null Safety

---
+++ fundamentos/src/main/kotlin/fundamentos/nullsafety.kt
@@ -0,0 +1,22 @@
+package fundamentos
+
+fun main(){
+
+    var lista : List<Int?> = listOf(1,2,null,4)
+    var listaNullable : List<Int>? = null
+//    var nome: String? = null
+//
+//    var tamanho: Any = nome?.length ?: "Digite o seu nome"
+//
+//    println(tamanho)
+
+//    var nome : String ? = "Lucas"
+//
+//    if(nome != null){
+//        println(nome.length)
+//    }
+//
+//    val toShort : Short = nome!!.length!!.toShort()
+
+
+}
