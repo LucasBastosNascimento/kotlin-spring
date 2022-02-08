6de575bb933a2898a7493b94dbc2bab3c397ee27
lucas bastos <lucas.bastos.nas@gmail.com>
2022-02-08 20:09:03 -0300

    wip(kotlin): secao 5 - utilizando o @RequestMapping para o end point

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,13 +1,15 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer")
 class CustomerController {
 
     @GetMapping
     fun helloWorld(): String{
-        return "Hello World"
+        return "Customer 1"
     }
 }
