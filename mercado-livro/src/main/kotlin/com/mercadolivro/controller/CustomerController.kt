c74352c7d94b7b197802a737f6f6ed635ed7bb14
lucas bastos <lucas.bastos.nas@gmail.com>
2022-02-23 19:55:52 -0300

    wip(kotlin): secao 5 - Implementando PUT

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,6 +1,7 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -32,5 +33,14 @@ class CustomerController {
     @GetMapping("/{id}")
     fun getCustomer(@PathVariable id: String): CustomerModel {
         return customers.filter { it.id ==id }.first()
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String,@RequestBody customer: PutCustomerRequest){
+        customers.filter { it.id ==id }.first().let {
+            it.name = customer.name
+            it.email = customer.email
+        }
     }
 }
