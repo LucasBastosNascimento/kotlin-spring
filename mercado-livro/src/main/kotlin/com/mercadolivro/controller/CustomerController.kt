559b7d79db9c0ae643debb83d69dd1f0e5c1bade
lucas bastos <lucas.bastos.nas@gmail.com>
2022-02-23 20:19:38 -0300

    wip(kotlin): secao 5 -Adicionando query param

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -13,7 +13,10 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getAll(): MutableList<CustomerModel> {
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name,true) }
+        }
         return customers
     }
 
