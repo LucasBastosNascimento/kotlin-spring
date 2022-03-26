50cc8e378998227dcd472c99906adbd0bfee442f
lucas bastos <lucas.bastos.nas@gmail.com>
2022-03-26 13:00:30 -0300

    wip(kotlin): secao 7 -Implementando e consumindo o banco de dados MySql

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -28,19 +28,19 @@ class CustomerController(
     }
 
     @GetMapping("/{id}")
-    fun getCustomer(@PathVariable id: String): CustomerModel {
+    fun getCustomer(@PathVariable id: Int): CustomerModel {
         return customerService.getCustomer(id)
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun update(@PathVariable id: String,@RequestBody customer: PutCustomerRequest){
+    fun update(@PathVariable id: Int,@RequestBody customer: PutCustomerRequest){
         customerService.update(customer.toCustomerModel(id))
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun delete(@PathVariable id: String){
+    fun delete(@PathVariable id: Int){
         customerService.delete(id)
     }
 }
--- mercado-livro/src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,7 +1,16 @@
 package com.mercadolivro.model
 
+import javax.persistence.*
+
+@Entity(name="customer")
 data class CustomerModel (
-    var id:String? = null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @Column
     var name: String,
+
+    @Column
     var email: String
         )
