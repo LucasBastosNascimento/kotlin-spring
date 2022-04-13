fb9f9243a86ec22005ef1758bab31c8481fd15e9
lucas bastos <lucas.bastos.nas@gmail.com>
2022-04-13 18:11:58 -0300

    wip(kotlin): secao 9 - Adicionando a validação aos paramentros

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutCustomerRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutCustomerRequest.kt
@@ -1,7 +1,12 @@
 package com.mercadolivro.controller.request
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+
 data class PutCustomerRequest (
 
+    @field:NotEmpty(message = "Nome deve ser informado")
     var name: String,
+    @field:Email(message = "E-mail deve ser valido")
     var email: String
         )
