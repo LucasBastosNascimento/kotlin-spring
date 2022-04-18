5f3e25d0beddda652a3523ce28c4f5e4f38352ee
lucas bastos <lucas.bastos.nas@gmail.com>
2022-04-18 18:53:36 -0300

    wip(kotlin): secao 11 - Implementando Swagger, Bean e Configurations

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -23,6 +23,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
 	implementation("org.flywaydb:flyway-core:8.5.4")
 	implementation("org.flywaydb:flyway-mysql:8.2.1")
 
