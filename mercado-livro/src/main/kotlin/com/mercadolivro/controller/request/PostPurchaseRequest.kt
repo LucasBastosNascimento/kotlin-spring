d014acf330baf97213ad21191ea2ed5bf8c2a142
lucas bastos <lucas.bastos.nas@gmail.com>
2022-04-21 00:11:18 -0300

    wip(kotlin): secao 12 - Disparando Eventos

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.controller.request
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
@@ -7,7 +8,10 @@ data class PostPurchaseRequest(
 
     @field:NotNull
     @field:Positive
+    @JsonAlias("customer_id")
     val customerId: Int,
+
+    @JsonAlias("books_id")
     @field:NotNull
     val booksId:Set<Int>
 )
