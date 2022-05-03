101d57cd525a64488e0243f94eb75199df87b94e
lucas bastos <lucas.bastos.nas@gmail.com>
2022-05-03 19:32:46 -0300

    wip(kotlin): secao 12 - Disparando Eventos

---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/events/listeners/UpdateSoldBookListener.kt
@@ -0,0 +1,21 @@
+package com.mercadolivro.events.listeners
+
+
+import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.service.PurchaseService
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class UpdateSoldBookListener(
+    private val purchaseService: PurchaseService
+) {
+
+    @EventListener
+    fun listen(purchaseEvent: PurchaseEvent){
+        val nfe = UUID.randomUUID().toString()
+        val purchaseModel = purchaseEvent.purchaseModel.copy(nfe=nfe)
+        purchaseService.update(purchaseModel)
+    }
+}
