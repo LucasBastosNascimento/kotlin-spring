5a1b3dbe43b6d0b2ae1326a3408f0716c78fda5d
lucas bastos <lucas.bastos.nas@gmail.com>
2022-05-05 19:55:05 -0300

    wip(kotlin): secao 13 - Criando tabela de Roles

--- mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.service
 
 import com.mercadolivro.enums.CustomerStatus
 import com.mercadolivro.enums.Errors
+import com.mercadolivro.enums.Profile
 import com.mercadolivro.execption.NotFoundException
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
@@ -20,7 +21,10 @@ class CustomerService(
     }
 
     fun create(customer: CustomerModel){
-        customerRepository.save(customer)
+        val customerCopy = customer.copy(
+            roles = setOf(Profile.CUSTOMER)
+        )
+        customerRepository.save(customerCopy)
     }
 
     fun findById(id: Int): CustomerModel {
