4fa44a9aaebbe3a46760ce29909f9a5f52662fba
lucas bastos <lucas.bastos.nas@gmail.com>
2022-06-30 14:25:49 -0300

    wip(kotlin): secao 13 - Adicionando dependecia do Spring Security e Criptografando Senha

--- mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,10 +2,29 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(): WebSecurityConfigurerAdapter() {
+
+    private val PUBLIC_MATCHERS = arrayOf<String>()
+
+    private val PUBLIC_POST_MATCHERS = arrayOf(
+        "/customers"
+    )
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable()
+        http.authorizeHttpRequests()
+            .antMatchers(*PUBLIC_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.POST,*PUBLIC_POST_MATCHERS).permitAll()
+            .anyRequest().authenticated()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder{
