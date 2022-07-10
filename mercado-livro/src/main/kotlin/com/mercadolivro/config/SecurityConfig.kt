3e7bb69ba63b216a0889db48cee6189df5f37d8c
lucas bastos <lucas.bastos.nas@gmail.com>
2022-07-10 15:25:42 -0300

    wip(kotlin): secao 13 - Adicionando @EnableWebSecutire

--- mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig(): WebSecurityConfigurerAdapter() {
 
     private val PUBLIC_MATCHERS = arrayOf<String>()
