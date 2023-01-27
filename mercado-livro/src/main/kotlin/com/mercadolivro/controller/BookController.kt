b8e9c1bdb98ad7f284ef232f6467c03783a31d9a
lucas bastos <lucas.bastos.nas@gmail.com>
2023-01-27 19:15:24 -0300

    wip(kotlin): secao 14 - Melhorando a Paginação

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -29,8 +29,8 @@ class BookController(
     }
 
     @GetMapping
-    fun findAll(@PageableDefault(page = 0, size = 10) pageable: Pageable): Page<BookResponse>{
-        return bookService.findAll(pageable).map { it.toResponse() }
+    fun findAll(@PageableDefault(page = 0, size = 10) pageable: Pageable): PageResponse<BookResponse> {
+        return bookService.findAll(pageable).map { it.toResponse() }.toPageResponse()
     }
 
     @GetMapping("/active")
---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/response/PageResponse.kt
@@ -0,0 +1,9 @@
+package com.mercadolivro.controller.response
+
+class PageResponse<T>(
+    var items: List<T>,
+    var currentPage: Int,
+    var totalItems: Long,
+    var totalPages: Int,
+
+)
