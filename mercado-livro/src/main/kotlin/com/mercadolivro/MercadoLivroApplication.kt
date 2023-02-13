c6ab6283b4b932a5d14e56ee793c2f6479ddbeaa
lucas bastos <lucas.bastos.nas@gmail.com>
2023-02-13 15:36:09 -0300

    wip(kotlin): secao 14 - Spring Profiles

--- mercado-livro/src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
@@ -11,5 +11,7 @@ class MercadoLivroApplication
 
 fun main(args: Array<String>) {
 	runApplication<MercadoLivroApplication>(*args)
-	//Para consultar o swagger utilise a url http://localhost:8080/swagger-ui/ 
+	//Para consultar o swagger utilise a url http://localhost:8080/swagger-ui/
+
+    // mudar na vw do -Dspring.profiles.active=prod
 }
