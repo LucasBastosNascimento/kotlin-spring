6c6382642d1f8d5995909dc7bd79e46e3be5269e
lucas bastos <lucas.bastos.nas@gmail.com>
2023-02-17 19:43:41 -0300

    wip(kotlin): secao 15 - Implementando testes com Junit e Mockk

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 
 	runtimeOnly("mysql:mysql-connector-java:")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.4")
 }
 
 tasks.withType<KotlinCompile> {
